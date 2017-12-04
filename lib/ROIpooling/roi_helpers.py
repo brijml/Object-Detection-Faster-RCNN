745fa44e005abd75497949648268ea4c620e06d7
Tejas Bobhate <tejasbobhte@Tejass-MacBook-Air.local>
2017-12-05 01:11:09 +0530

    fixed some bugs, some more to fix

--- lib/ROIpooling/roi_helpers.py
+++ lib/ROIpooling/roi_helpers.py
@@ -1,26 +1,32 @@
 import numpy as np
-import cv2,os
+import cv2
+import os
 import xml.etree.ElementTree as ET
 from random import shuffle
 import matplotlib.pyplot as plt
+
 
 def show(img):
     plt.imshow(img)
     plt.show()
     return
 
+
 def parse(xml_file):
-    classes, boxes = [],[]
+    classes, boxes = [], []
     tree = ET.parse(xml_file)
     root = tree.getroot()
     for child in root:
         if child.tag == 'object':
             name = child[0].text
+            # print('name', name)
+            # print('1', child[4][0].text, child[4][1].text, child[4][2].text, child[4][3].text)
             x1, y1, x2, y2 = int(child[4][0].text), int(child[4][1].text), int(child[4][2].text), int(child[4][3].text)
             classes.append(name)
             boxes.append([x1, y1, x2, y2])
 
     return classes, boxes
+
 
 def intersection_over_union_score(boxA, boxB):
     # determine the (x, y)-coordinates of the intersection rectangle
@@ -32,17 +38,17 @@ def intersection_over_union_score(boxA, boxB):
     # compute the area of intersection rectangle
     interArea = (xB - xA + 1) * (yB - yA + 1)
 
-    # compute the area of both the prediction and ground-truth
-    # rectangles
     boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
     boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
 
-    # compute the intersection over union by taking the intersection
-    # area and dividing it by the sum of prediction + ground-truth
-    # areas - the interesection area
-    iou = interArea / float(boxAArea + boxBArea - interArea)
+    try:
+        iou = interArea / float(boxAArea + boxBArea - interArea)
+    except Exception as e:
+        iou = 0
 
-    # return the intersection over union value
+    if iou < 0:
+        iou = 0
+
     return iou
 
 
@@ -54,14 +60,16 @@ def resize(image, boxes, arr):
     m, n = image.shape[:2]
     a = float(m) / n
     if m > n:
-        m1, n1 = 1000, int(1000 / a)
+        # m1, n1 = 1000, int(1000 / a)
+        m1, n1 = 1000, 710
     else:
-        m1, n1 = int(1000 * a), 1000
+        # m1, n1 = int(1000 * a), 1000
+        m1, n1 = 710, 1000
     rm, rn = int(m1 / m), int(n1 / n)
     image = cv2.resize(image, (n1, m1))
 
     # applying same resize to label co-ordinates, the co-ordinates are in convention (x,y) and not (row,column)
-    for i,box in enumerate(boxes):
+    for i, box in enumerate(boxes):
         boxes[i] = [int(box[0] * rn), int(box[1] * rm), int(box[2] * rn), int(box[3] * rm)]
 
     # roi_height = arr[:,2]-arr[:,0]
--- lib/ROIpooling/train_fastrcnn.py
+++ lib/ROIpooling/train_fastrcnn.py
@@ -1,135 +1,152 @@
 import numpy as np
 from roi_helpers import *
 import scipy.io as spio
+# from random import shuffle
 
 POOL_SIZE = 7
 DS_FACTOR = 16
 
+
 def visualise_minibatch(img, batch):
     gboxes, pboxes = batch[1], batch[2]
-    print len(gboxes), len(pboxes)
+    # print len(gboxes), len(pboxes)
+    # print(len(gboxes), len(pboxes))
     for box in gboxes:
         if box is not None:
-            x1,y1,x2,y2 = box[0], box[1], box[2], box[3]
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2) 
+            x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     for box in pboxes:
         if box is not None:
-            x1,y1,x2,y2 = box[0], box[1], box[2], box[3]
-            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0),2) 
+            x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-    show(img)
+    # show(img)
     return
- 
+
+
 def one_hot(classes):
 
-    probability = np.zeros((len(classes),21), dtype=np.uint8)
-    for i,class_ in enumerate(classes):
-        probability[i,class_dict[class_]] = 1
+    probability = np.zeros((len(classes), 21), dtype=np.uint8)
+    for i, class_ in enumerate(classes):
+        probability[i, class_dict[class_]] = 1
 
     return probability
 
+
 def parameterise(gbox, pbox):
-    box = np.zeros(4, dtype = np.float32)
-    pw,ph = pbox[2]-pbox[0], pbox[3]-pbox[1]
-    gw,gh = gbox[2]-gbox[0], gbox[3]-gbox[1]
-    box[0] = (gbox[0]-pbox[0])/pw
-    box[1] = (gbox[1]-pbox[1])/ph
-    box[2] = np.log(float(gw)/pw)
-    box[3] = np.log(float(gh)/ph)
+    box = np.zeros(4, dtype=np.float32)
+    pw, ph = pbox[2] - pbox[0], pbox[3] - pbox[1]
+    gw, gh = gbox[2] - gbox[0], gbox[3] - gbox[1]
+    box[0] = (gbox[0] - pbox[0]) / pw
+    box[1] = (gbox[1] - pbox[1]) / ph
+    box[2] = np.log(float(gw) / pw)
+    box[3] = np.log(float(gh) / ph)
     return box
+
 
 def create_array(classes, gboxes, pboxes):
     cls_array = one_hot(classes)
     bboxes_array = np.zeros((len(gboxes), 80), dtype=np.float32)
 
-    for i,gbox in enumerate(gboxes):
+    for i, gbox in enumerate(gboxes):
         if gbox is not None:
             parametrised_box = parameterise(gbox, pboxes[i])
-            start_idx = 4*class_dict[classes[i]]
-            bboxes_array[i, start_idx:start_idx+4] = parametrised_box 
+            # print("para box shape", parametrised_box.shape)
+            start_idx = 4 * (class_dict[classes[i]] - 1)
+            # print('start id', start_idx)
+            bboxes_array[i, start_idx:start_idx + 4] = parametrised_box
 
-    return cls_array,bboxes_array
+    return cls_array, bboxes_array
 
 
 def sample_minibatch(class_labels, gt_boxes, proposals_image):
-    classes, bboxes, pboxes = [],[],[]
-    
+    classes, bboxes, pboxes = [], [], []
+
     pcount = 0
-    while pcount<16:
+    while pcount < 16:
         idx = np.random.randint(len(proposals_image))
         proposal = proposals_image[idx]
-        proposal_width,proposal_height = proposal[3]-proposal[1], proposal[2]-proposal[0]
-        if proposal_width>DS_FACTOR*POOL_SIZE and proposal_height>DS_FACTOR*POOL_SIZE:
-            for i,gt_box in enumerate(gt_boxes):
+        proposal_width, proposal_height = proposal[3] - proposal[1], proposal[2] - proposal[0]
+        if proposal_width > DS_FACTOR * POOL_SIZE and proposal_height > DS_FACTOR * POOL_SIZE:
+            for i, gt_box in enumerate(gt_boxes):
                 iou = intersection_over_union_score(gt_box, proposal)
                 if iou > 0.5:
                     classes.append(class_labels[i])
                     bboxes.append(gt_box)
                     pboxes.append(proposal)
-                    pcount+=1
+                    pcount += 1
 
-    ncount = 0 
-    while ncount<48:
+    ncount = 0
+    while ncount < 48:
         idx = np.random.randint(len(proposals_image))
         proposal = proposals_image[idx]
-        proposal_width,proposal_height = proposal[3]-proposal[1], proposal[2]-proposal[0]
-        if proposal_width>DS_FACTOR*POOL_SIZE and proposal_height>DS_FACTOR*POOL_SIZE:
-            for i,gt_box in enumerate(gt_boxes):
+        proposal_width, proposal_height = proposal[3] - proposal[1], proposal[2] - proposal[0]
+        if proposal_width > DS_FACTOR * POOL_SIZE and proposal_height > DS_FACTOR * POOL_SIZE:
+            for i, gt_box in enumerate(gt_boxes):
                 iou = intersection_over_union_score(gt_box, proposal)
-                if iou>=0.1 and iou<0.5:
+                if iou >= 0.1 and iou < 0.5:
                     classes.append('background')
                     bboxes.append(None)
                     pboxes.append(None)
-                    ncount+=1
+                    ncount += 1
 
-    rand_idx = range(len(pboxes))
+    rand_idx = list(range(len(pboxes)))
     shuffle(rand_idx)
-    
-    temp_classes, temp_bboxes, temp_pboxes = [],[],[]
+
+    temp_classes, temp_bboxes, temp_pboxes = [], [], []
     for i in rand_idx:
         temp_bboxes.append(bboxes[i])
         temp_classes.append(classes[i])
         temp_pboxes.append(pboxes[i])
 
-    return classes,bboxes,pboxes
+    return classes, bboxes, pboxes
+
 
 def train_on_batch(batch_class, batch_boxes):
-	pass
-	
+    pass
+
+
 if __name__ == '__main__':
-	all_proposals = spio.loadmat('/home/brij/selective_search_data/voc_2007_trainval.mat')
-	class_dict = {"background":0,"aeroplane":1,"bicycle":2, "bird":3, "boat":4, "bottle":5, "bus":6,"car":7,
-					"cat":8, "chair":9, "cow":10, "diningtable":11, "dog":12, "horse":13, "motorbike":14,
-					"person":15, "pottedplant":16, "sheep":17, "sofa":18, "train":19, "tvmonitor":20}
-	images_dir = '/home/brij/Desktop/github_projects/obj_det/datasets/VOCdevkit/VOC2007/JPEGImages/'
-	annotations_dir = '/home/brij/Desktop/github_projects/obj_det/datasets/VOCdevkit/VOC2007/Annotations/'
-	files = os.listdir(images_dir)
-	N,R = 2,64
-	no_epochs = 10
-	epoch = 0
 
-	while epoch<no_epochs:
-		shuffle(files)
-		for i in range(0,len(files),N):
-			files_batch = files[i:i+N]
-			batch_class, batch_boxes, batch_pboxes = [],[],[]
-			imgs = []
-			for file in files_batch:
-				name = file.split('.')[0]
-				img = cv2.imread(os.path.join(images_dir,file))
-				xml_file = os.path.join(annotations_dir,name+'.xml')
-				class_labels, gt_boxes = parse(xml_file)
-				index = np.where(all_proposals['images'] == name)[0][0]
-				proposals_image = all_proposals['boxes'][:, index][0]
-				img, gt_boxes, proposals_image = resize(img, gt_boxes, proposals_image)
-		
-				batch = sample_minibatch(class_labels, gt_boxes, proposals_image)
-				visualise_minibatch(img, batch)
-				batch_class.extend(batch[0]); batch_boxes.extend(batch[1]); batch_pboxes.extend(batch[2])
-				imgs.append(img)
+    all_proposals = spio.loadmat('/home/brij/selective_search_data/voc_2007_trainval.mat')
+    class_dict = {"background": 0, "aeroplane": 1, "bicycle": 2, "bird": 3, "boat": 4, "bottle": 5, "bus": 6, "car": 7,
+                  "cat": 8, "chair": 9, "cow": 10, "diningtable": 11, "dog": 12, "horse": 13, "motorbike": 14,
+                  "person": 15, "pottedplant": 16, "sheep": 17, "sofa": 18, "train": 19, "tvmonitor": 20}
+    images_dir = '/home/brij/Desktop/github_projects/obj_det/datasets/VOCdevkit/VOC2007/JPEGImages/'
+    annotations_dir = '/home/brij/Desktop/github_projects/obj_det/datasets/VOCdevkit/VOC2007/Annotations/'
+    files = os.listdir(images_dir)
+    N, R = 2, 64
+    no_epochs = 10
+    epoch = 0
 
-			imgs_array = np.array(imgs)
-			batch_class_array, batch_boxes_array = create_array(batch_class, batch_boxes, batch_pboxes)
-			# train_on_batch(imgs_array, batch_class_array, batch_boxes_array)
-		epoch+=1
+    while epoch < no_epochs:
+        shuffle(files)
+        for i in range(0, len(files), N):
+            # print('file no', i)
+            files_batch = files[i:i + N]
+            batch_class, batch_boxes, batch_pboxes = [], [], []
+            imgs = []
+            for file in files_batch:
+                name = file.split('.')[0]
+                img = cv2.imread(os.path.join(images_dir, file))
+                xml_file = os.path.join(annotations_dir, name + '.xml')
+                class_labels, gt_boxes = parse(xml_file)
+                index = np.where(all_proposals['images'] == name)[0][0]
+                proposals_image = all_proposals['boxes'][:, index][0]
+                img, gt_boxes, proposals_image = resize(img, gt_boxes, proposals_image)
+                # print('img shape', i, img.shape)
+
+                batch = sample_minibatch(class_labels, gt_boxes, proposals_image)
+                visualise_minibatch(img, batch)
+                batch_class.extend(batch[0])
+                batch_boxes.extend(batch[1])
+                batch_pboxes.extend(batch[2])
+                imgs.append(img)
+
+            imgs_array = np.array(imgs)
+            # print('batch classes', len(batch_class))
+            batch_class_array, batch_boxes_array = create_array(batch_class, batch_boxes, batch_pboxes)
+            # train_on_batch(imgs_array, batch_class_array, batch_boxes_array)
+        epoch += 1
+        # print('epoch', epoch)
